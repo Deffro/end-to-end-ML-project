1019b0e5f515a866c9dfdca6eb3d6d359a85ec24
Deffro <dimitris.effrosynidis@gmail.com>
2022-03-08 11:11:56 +0200

    pipeline added

---
+++ notebooks/.ipynb_checkpoints/preprocess-checkpoint.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class Mapper(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables, mappings):
+
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+
+        self.variables = variables
+        self.mappings = mappings
+
+    def fit(self, X, y=None):
+        # fit statement to be in-line with the sklearn pipeline
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.mappings)
+
+        return X
